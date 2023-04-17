1c5d61c97bb900fa6b9d6deeee7eb6ffd2126d20
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-04-17 16:30:10 +0300

    add home, loginForm Layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,62 @@
-import { ContactForm } from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
-import { Filter } from './Filter/Filter';
-import {
-  StyledTitle,
-  StyledContainer,
-  StyledContactsTitle,
-} from './App.styled';
-import { selectError, selectIsLoading } from 'redux/contacts/selectors';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { fetchContacts } from 'redux/contacts/operations';
+// import { ContactForm } from './ContactForm/ContactForm';
+// import { ContactList } from './ContactList/ContactList';
+// import { Filter } from './Filter/Filter';
+// import {
+//   StyledTitle,
+//   StyledContainer,
+//   StyledContactsTitle,
+// } from './App.styled';
+import { Route, Routes } from 'react-router-dom';
+import { lazy, useEffect } from 'react';
+import { Layout } from './Layout/Layout';
+import { RestrictedRoute } from './RestrictedRoute/RestrictedRoute';
+import { useDispatch } from 'react-redux';
+import { useAuth } from 'hooks/useAuth';
+import { refreshUser } from 'redux/auth/operations';
+const Login = lazy(() => import('../../src/pages/Login/Login'));
+const Home = lazy(() => import('../../src/pages/Home/Home'));
+
 
 export function App() {
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
-  const dispatch = useDispatch();
+  // const isLoading = useSelector(selectIsLoading);
+  // const error = useSelector(selectError);
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch(fetchContacts());
+  // }, [dispatch]);
+    const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
 
   useEffect(() => {
-    dispatch(fetchContacts());
+    dispatch(refreshUser());
   }, [dispatch]);
 
   return (
-    <StyledContainer>
-      <StyledTitle>Phonebook</StyledTitle>
+    <div>
+      {isRefreshing ? (
+        <b>Refreshing user...</b>
+      ) : (<Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute component={Login} redirectTo="/contacts" />
+            }
+          />
+        </Route>
+      </Routes>
+      )}
+      {/* <StyledTitle>Phonebook</StyledTitle>
       <ContactForm />
 
       <StyledContactsTitle>Contacts</StyledContactsTitle>
-      <Filter />
-      {isLoading && !error && <b>Request in progress...</b>}
-      <ContactList />
-    </StyledContainer>
+      <Filter /> */}
+      {/* {isLoading && !error && <b>Request in progress...</b>} */}
+      {/* <ContactList /> */}
+
+
+    </div>
   );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+// import { StyledContainer, StyledTitle } from "components/App.styled";
+
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <p>Welcome!</p>
+    </div>
+  );
+}
+
+export default Home;
