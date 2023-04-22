2bf77dcfcbe3762eda154055c1b5d208e4eb1d5c
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-04-22 16:34:58 +0300

    Update RegisterForm.jsx

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,35 +1,47 @@
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { register } from 'redux/auth/operations';
 
 export const RegisterForm = () => {
   const dispatch = useDispatch();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'name':
+        return setName(value);
+      case 'email':
+        return setEmail(value);
+      case 'password':
+        return setPassword(value);
+      default:
+        return;
+    }
+  };
 
   const handleSubmit = e => {
     e.preventDefault();
-    const form = e.currentTarget;
-    dispatch(
-      register({
-        name: form.elements.name.value,
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    );
-    form.reset();
+    dispatch(register({ name, email, password }));
+    setName('');
+    setEmail('');
+    setPassword('');
   };
 
   return (
     <form onSubmit={handleSubmit} autoComplete="off">
       <label>
         Username
-        <input type="text" name="name" />
+        <input type="text" name="name" onChange={handleChange} />
       </label>
       <label>
         Email
-        <input type="email" name="email" />
+        <input type="email" name="email" onChange={handleChange} />
       </label>
       <label>
         Password
-        <input type="password" name="password" />
+        <input type="password" name="password" onChange={handleChange} />
       </label>
       <button type="submit">Register</button>
     </form>
