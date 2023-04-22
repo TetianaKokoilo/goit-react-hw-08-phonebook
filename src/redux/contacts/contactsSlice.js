134adee82a9191b915d430f614e25c2ef669c508
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-04-22 22:08:47 +0300

    Update contactsSlice.js

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -36,8 +36,7 @@ const contactsSlice = createSlice({
       state.isLoading = false;
       state.error = null;
       const index = state.items.findIndex(
-        contact => contact.id === action.payload.id
-      );
+        item => item.id === action.payload);
       state.items.splice(index, 1);
     },
     [deleteContact.rejected]: handleRejected,
