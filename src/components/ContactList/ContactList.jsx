74d07e641196208da91f14d5f346c32ea29b27f0
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-04-29 22:23:37 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,19 +2,37 @@
 import { deleteContact } from '../../redux/contacts/operations';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectFilteredContacts } from '../../redux/contacts/selectors';
-import { List, ListItem, ListItemText } from '@mui/material';
+import { Button, List, ListItem, ListItemText, Stack } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export const ContactList = () => {
   const contacts = useSelector(selectFilteredContacts);
   const dispatch = useDispatch();
   return (
-    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+    <List
+      sx={{
+        width: '100%',
+        maxWidth: 360,
+        bgcolor: 'background.paper',
+        justifyContent: 'center',
+      }}
+    >
       {contacts.map(({ id, name, number }) => (
         <ListItem key={id}>
           <ListItemText primary={name} secondary={number} />
-          <button type="button" onClick={() => dispatch(deleteContact(id))}>
-            Delete
-          </button>
+          <Stack direction="row" spacing={2}>
+            <Button
+              variant="outlined"
+              type="button"
+              onClick={() => dispatch(deleteContact(id))}
+              startIcon={<DeleteIcon />}
+            >
+              Delete
+            </Button>
+            {/* <button type="button" onClick={() => dispatch(deleteContact(id))}>
+              Delete
+            </button> */}
+          </Stack>
         </ListItem>
       ))}
     </List>
